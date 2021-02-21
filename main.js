9c5c89b992879ff8751650f3ec1dc6ad90becd5c
Shawn M (he/him) <shawnmcmahon17@gmail.com>
2021-02-20 18:42:12 -0700

    Add querySelectors

---
+++ main.js
@@ -0,0 +1,6 @@
+//querySelectors
+var affirmationSelected = document.querySelector('input[value="affirmation"]');
+var mantraSelected = document.querySelector('input[value="mantra"]');
+var receiveMessageButton = document.querySelector('.receive-message');
+var placeholderImage = document.querySelector('.default-placeholder-image');
+var message = document.querySelector('.message');
